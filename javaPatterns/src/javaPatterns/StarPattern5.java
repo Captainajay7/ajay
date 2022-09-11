d83eabff549b27024fe9707a56e4e17b46cff544
ajay <ajay@MSI>
2022-09-11 18:19:35 +0530

    JAV PATTERNS

---
+++ javaPatterns/src/javaPatterns/StarPattern5.java
@@ -0,0 +1,21 @@
+package javaPatterns;
+
+public class StarPattern5 {
+
+	public static void main(String[] args) {
+          
+		int count = 0;
+		for (int i=1;i<=4;i++) {
+			for (int j =3;j>=i;j--) {
+				System.out.print(" ");
+			} 	for (int k =1;k<=i;k++) {
+				count = count +1;
+				System.out.print(count);
+			}
+		
+			System.out.println(" ");
+		}
+
+	}
+
+}
